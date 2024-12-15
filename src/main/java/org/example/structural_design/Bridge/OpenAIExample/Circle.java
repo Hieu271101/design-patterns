d0b5e33b86d0c197b480686d60f018e5c9301fa3
Hieu Do <hieu.do@aeris.net>
2024-12-15 22:45:07 +0700

    fix bridge

---
+++ src/main/java/org/example/structural_design/Bridge/OpenAIExample/Circle.java
@@ -0,0 +1,17 @@
+package org.example.structural_design.Bridge.OpenAIExample;
+
+public class Circle extends Shape{
+    private int x;
+    private int y;
+    private int radius;
+    public Circle(int x, int y, int radius, DrawAPI drawAPI){
+        super(drawAPI);
+        this.x = x;
+        this.y = y;
+        this.radius = radius;
+    }
+    @Override
+    public void draw() {
+
+    }
+}
